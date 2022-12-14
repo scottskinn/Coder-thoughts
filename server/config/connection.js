071ef739ca42c4b91526d038ffd4774fa3f828ff
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-12-14 17:14:17 -0500

    updates

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1/coder-thoughts',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
